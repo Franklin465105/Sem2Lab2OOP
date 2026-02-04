6fb0c31f4ac62f86b5e5302ea2d9c8dfaedf9976
Franklin Esedebe <G00465105@atu.ie>
2026-02-04 16:20:53 +0000

    Created project

---
+++ src/main/java/ie/atu/sem2lab2oop/Sem2Lab2OopApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.sem2lab2oop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem2Lab2OopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem2Lab2OopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/sem2lab2oop/Sem2Lab2OopApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.sem2lab2oop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem2Lab2OopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
